360d5262c070a36ded076e8f2a80890385196316
Rajdeepak S <106483490+Rajdeepak29@users.noreply.github.com>
2024-12-06 23:55:48 +0530

    vercel

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE,PATCH, OPIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // User routes
 app.use("/api", userRoutes);
 
